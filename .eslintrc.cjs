f8e301d57bd779e1c3e1df7e72590c7e3cf1833a
LeonardMaetzner <moneybefehl@gmail.com>
2024-08-17 20:13:04 +0200

    chnage from error on unused variables to warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
     },
     plugins: ['react-refresh'],
     rules: {
+      'no-unused-vars': 'warn',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
